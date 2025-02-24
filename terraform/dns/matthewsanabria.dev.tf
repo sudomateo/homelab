fe0a2803ea46b04e4288cb3adda1142a4ad69a55
Matthew Sanabria <me@matthewsanabria.dev>
2025-02-23 19:58:22 -0500

    dns: add www cname record

--- terraform/dns/matthewsanabria.dev.tf
+++ terraform/dns/matthewsanabria.dev.tf
@@ -2,16 +2,20 @@ data "cloudflare_zone" "matthewsanabria_dev" {
   zone_id = "92040175fb0287b5af9129a410275e5c"
 }
 
-import {
-  id = "${data.cloudflare_zone.matthewsanabria_dev.zone_id}/bc69d83426d3f1e3c354337368ef97c1"
-  to = cloudflare_dns_record.matthewsanabria_dev_a
-}
-
 resource "cloudflare_dns_record" "matthewsanabria_dev_a" {
   zone_id = data.cloudflare_zone.matthewsanabria_dev.zone_id
   type    = "A"
   name    = "@"
   content = var.public_ipv4
+  proxied = true
+  ttl     = 1 # Automatic TTL.
+}
+
+resource "cloudflare_dns_record" "www_matthewsanabria_dev_cname" {
+  zone_id = data.cloudflare_zone.matthewsanabria_dev.zone_id
+  type    = "CNAME"
+  name    = "www"
+  content = data.cloudflare_zone.matthewsanabria_dev.name
   proxied = true
   ttl     = 1 # Automatic TTL.
 }
