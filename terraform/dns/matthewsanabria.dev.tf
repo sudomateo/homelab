6b40db0b92110367c778ed67534effb08eb5ae77
Matthew Sanabria <me@matthewsanabria.dev>
2025-02-23 19:39:24 -0500

    dns: fix zone data source

--- terraform/dns/matthewsanabria.dev.tf
+++ terraform/dns/matthewsanabria.dev.tf
@@ -1,16 +1,14 @@
 data "cloudflare_zone" "matthewsanabria_dev" {
-  filter {
-    name = "matthewsanabria.dev"
-  }
+  zone_id = "92040175fb0287b5af9129a410275e5c"
 }
 
 import {
-  id = "${data.cloudflare_zone.matthewsanabria_dev}/bc69d83426d3f1e3c354337368ef97c1"
+  id = "${data.cloudflare_zone.matthewsanabria_dev.zone_id}/bc69d83426d3f1e3c354337368ef97c1"
   to = cloudflare_dns_record.matthewsanabria_dev_a
 }
 
 resource "cloudflare_dns_record" "matthewsanabria_dev_a" {
-  zone_id = data.cloudflare_zone.matthewsanabria_dev
+  zone_id = data.cloudflare_zone.matthewsanabria_dev.zone_id
   type    = "A"
   name    = "@"
   content = var.public_ipv4
