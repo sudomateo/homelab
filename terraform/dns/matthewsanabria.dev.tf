6af13f2e317a9426d66b466039f7396c46ab3447
Matthew Sanabria <me@matthewsanabria.dev>
2025-02-23 20:03:53 -0500

    dns: add comment to records

--- terraform/dns/matthewsanabria.dev.tf
+++ terraform/dns/matthewsanabria.dev.tf
@@ -1,9 +1,14 @@
+locals {
+  comment = "Managed by Home Lab Terraform Cloud project."
+}
+
 data "cloudflare_zone" "matthewsanabria_dev" {
   zone_id = "92040175fb0287b5af9129a410275e5c"
 }
 
 resource "cloudflare_dns_record" "matthewsanabria_dev_a" {
   zone_id = data.cloudflare_zone.matthewsanabria_dev.zone_id
+  comment = local.comment
   type    = "A"
   name    = "@"
   content = var.public_ipv4
@@ -13,6 +18,7 @@ resource "cloudflare_dns_record" "matthewsanabria_dev_a" {
 
 resource "cloudflare_dns_record" "www_matthewsanabria_dev_cname" {
   zone_id = data.cloudflare_zone.matthewsanabria_dev.zone_id
+  comment = local.comment
   type    = "CNAME"
   name    = "www"
   content = data.cloudflare_zone.matthewsanabria_dev.name
